f26090b550245015ea0b6a8b66e74cc855f27101
binghu <365356015@qq.com>
2020-04-12 14:05:25 +0800

    导出SVGRenderer和BitmapAdapter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const RenderWebGL = require('./RenderWebGL');
+const SvgRenderer = require('./scratch-svg-renderer');
 
 /**
  * Export for NPM & Node.js
  * @type {RenderWebGL}
  */
 module.exports = RenderWebGL;
+
+// 导出SVGRenderer, scratch-gui中用得上
+module.exports.SVGRenderer = SvgRenderer.SVGRenderer;
+
+// 导出BitmapAdapter, scratch-gui中用得上
+module.exports.BitmapAdapter = SvgRenderer.BitmapAdapter;
